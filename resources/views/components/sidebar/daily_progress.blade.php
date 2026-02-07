3eb43fa6c31beb29e907b8342b8707c77dc507c8
unknown <abdelhadeeladawy@gmail.com>
2026-02-07 14:17:33 +0200

    Fix route names in daily_progress sidebar - add progress. prefix to all routes

--- resources/views/components/sidebar/daily_progress.blade.php
+++ resources/views/components/sidebar/daily_progress.blade.php
@@ -1,52 +1,52 @@
 <li class="nav-item">
-    <a class="nav-link" href="{{ route('project_types.index') }}">
+    <a class="nav-link" href="{{ route('progress.project_types.index') }}">
         <i class="ti-control-record"></i>{{ __('navigation.project_types') }}
     </a>
 </li>
 <li class="nav-item">
-    <a class="nav-link" href="{{ route('clients.index') }}">
+    <a class="nav-link" href="{{ route('progress.clients.index') }}">
         <i class="ti-control-record"></i>{{ __('navigation.clients') }}
     </a>
 </li>
 <li class="nav-item">
-    <a class="nav-link" href="{{ route('employees.index') }}">
+    <a class="nav-link" href="{{ route('progress.employees.index') }}">
         <i class="ti-control-record"></i>{{ __('navigation.employees') }}
     </a>
 </li>
 <li class="nav-item">
-    <a class="nav-link" href="{{ route('work.items.index') }}">
+    <a class="nav-link" href="{{ route('progress.work-items.index') }}">
         <i class="ti-control-record"></i>{{ __('navigation.work_items') }}
     </a>
 </li>
 <li class="nav-item">
-    <a class="nav-link" href="{{ route('work-item-categories.index') }}">
+    <a class="nav-link" href="{{ route('progress.categories.index') }}">
         <i class="ti-control-record"></i>تصنيفات بنود الأعمال
     </a>
 </li>
 <li class="nav-item">
-    <a class="nav-link" href="{{ route('issues.index') }}">
+    <a class="nav-link" href="{{ route('progress.issues.index') }}">
         <i class="ti-control-record"></i>issues
     </a>
 </li>
 <li class="nav-item">
-    <a class="nav-link" href="{{ route('item-statuses.index') }}">
+    <a class="nav-link" href="{{ route('progress.item-statuses.index') }}">
         <i class="ti-control-record"></i>Item Statuses
     </a>
 </li>
 <li class="nav-item">
-    <a class="nav-link" href="{{ route('project.template.index') }}">
+    <a class="nav-link" href="{{ route('progress.project-templates.index') }}">
         <i class="ti-control-record"></i>{{ __('navigation.project_template') }}
     </a>
 </li>
 
 <li class="nav-item">
-    <a class="nav-link" href="{{ route('progress.project.index') }}">
+    <a class="nav-link" href="{{ route('progress.projects.index') }}">
         <i class="ti-control-record"></i>{{ __('navigation.projects') }}
     </a>
 </li>
 
 <li class="nav-item">
-    <a class="nav-link" href="{{ route('daily_progress.index') }}">
+    <a class="nav-link" href="{{ route('progress.daily-progress.index') }}">
         <i class="ti-control-record"></i>{{ __('navigation.daily_progress') }}
     </a>
 </li>
