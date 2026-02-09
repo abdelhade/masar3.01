534b5287fb8870c8e517bc0dc0cfdf8f82965ff4
MohamedElagawy <mohamedelagawy49@gmail.com>
2026-02-09 23:35:57 +0200

    update in invoice module

--- Modules/Invoices/Resources/views/invoices/edit.blade.php
+++ Modules/Invoices/Resources/views/invoices/edit.blade.php
@@ -1,13 +1,8 @@
 @extends('admin.dashboard')
 
-@section('body_class', 'invoice-page')
-@section('hide_footer')
-@endsection
-@push('styles')
-    @include('admin.partials.invoice-page-styles')
-@endpush
+{{-- Dynamic Sidebar --}}
 @section('sidebar')
-    @if (in_array($invoice->type->id, [10, 12, 14, 16, 22, 26]))
+    @if (in_array($invoice->type->id, [10, 12, 14, 16, 22]))
         @include('components.sidebar.sales-invoices')
     @elseif (in_array($invoice->type->id, [11, 13, 15, 17, 24, 25]))
         @include('components.sidebar.purchases-invoices')
@@ -16,5 +11,7 @@
     @endif
 @endsection
 @section('content')
-    <livewire:invoices.edit-invoice-form :operationId="$invoice->id" />
+    <div class="container">
+        <livewire:invoices.edit-invoice-form :operationId="$invoice->id" />
+    </div>
 @endsection
